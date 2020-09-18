d63ca14da76419a41b50672d2214c72b48f17732
Malte Splietker <malte.splietker@gmail.com>
2020-09-18 11:22:21 +0200

    use float type voxels by default

--- InfiniTAM/ITMLib/ITMLibDefines.h
+++ InfiniTAM/ITMLib/ITMLibDefines.h
@@ -15,10 +15,9 @@ typedef ITMLib::ITMSurfel_rgb ITMSurfelT;
 /** This chooses the information stored at each voxel. At the moment, valid
     options are ITMVoxel_s, ITMVoxel_f, ITMVoxel_s_rgb and ITMVoxel_f_rgb.
 */
-typedef ITMVoxel_s_rgb ITMVoxel;
+typedef ITMVoxel_f_rgb ITMVoxel;
 
 /** This chooses the way the voxels are addressed and indexed. At the moment,
     valid options are ITMVoxelBlockHash and ITMPlainVoxelArray.
 */
 typedef ITMLib::ITMVoxelBlockHash ITMVoxelIndex;
-//typedef ITMLib::ITMPlainVoxelArray ITMVoxelIndex;
